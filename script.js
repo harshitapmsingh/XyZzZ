db135b74964ed13add8f63b4afbdcd9670faca2f
harshitapmsingh <43518148+harshitapmsingh@users.noreply.github.com>
2020-05-25 00:07:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+var endDate = new Date("Dec 6, 2020 00:00:00").getTime();
+
+var timer = setInterval(function() {
+
+    let now = new Date().getTime();
+    let t = endDate - now;
+    
+    if (t >= 0) {
+    
+        let days = Math.floor(t / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor((t % (1000 * 60)) / 1000);
+    
+        document.getElementById("timer-days").innerHTML = days + "<span class='label'>Days</span>";
+    
+        document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2) + "<span class='label'>Hours</span>";
+    
+        document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) + "<span class='label'>Minutes</span>";
+    
+        document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2) + "<span class='label'>Seconds</span>";
+		document.getElementById("bottom").innerHTML = "<br>Until Atticus' Birthday";
+		
+    } else {
+        document.getElementById("timer").innerHTML = "Happy Birthday, Atticus!";
+		document.getElementById("botton").innerHTML = "";
+    }
+    
+}, 1000);
